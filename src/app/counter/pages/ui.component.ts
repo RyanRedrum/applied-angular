5db8c5b83887c6f3d311144b93de555d603d5705
swee.ryan@gmail.com <swee.ryan@gmail.com>
2025-01-29 11:00:02 -0500

    FizzBuzz.

--- src/app/counter/pages/ui.component.ts
+++ src/app/counter/pages/ui.component.ts
@@ -9,22 +9,46 @@ import {
   selector: 'app-ui',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ` <div>
-    <button
-      class="btn btn-primary"
-      (click)="decrement()"
-      [disabled]="!canDecrement()"
-    >
-      -
-    </button>
-    <span data-testid="current">{{ count() }}</span>
-    <button class="btn btn-primary" (click)="increment()">+</button>
-  </div>`,
+  template: `
+    <div>
+      <button
+        class="btn btn-primary"
+        (click)="decrement()"
+        [disabled]="!canDecrement()"
+      >
+        -
+      </button>
+      <span data-testid="current">{{ count() }}</span>
+      <button class="btn btn-primary" (click)="increment()">+</button>
+    </div>
+    <div>
+      <span data-testid="fizzBuzz">{{ fizzBuzz() }}</span>
+    </div>
+  `,
   styles: ``,
 })
 export class UiComponent {
   count = signal<number>(0);
   canDecrement = computed(() => this.count() > 0);
+  fizzBuzz = computed(() => {
+    const currentCount = this.count();
+    if (currentCount === 0) {
+      return '';
+    }
+
+    const fizz = currentCount % 3 === 0;
+    const buzz = currentCount % 5 === 0;
+
+    if (fizz && buzz) {
+      return 'FizzBuzz';
+    } else if (fizz) {
+      return 'Fizz';
+    } else if (buzz) {
+      return 'Buzz';
+    }
+
+    return '';
+  });
 
   increment = () => {
     this.count.update(() => this.count() + 1);
